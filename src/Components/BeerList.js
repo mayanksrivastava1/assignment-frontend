233f548ee18400175a061fa790ce117c3e5ddd96
mayanksrivastava1 <100144534+mayanksrivastava1@users.noreply.github.com>
2023-08-22 16:44:10 +0530

    assignment-react

---
+++ src/Components/BeerList.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+
+function BeerList() {
+  const [beers, setBeers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  
+  useEffect(() => {
+    fetch(`https://api.punkapi.com/v2/beers`)
+      .then(response => response.json())
+      .then(data => setBeers(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const filteredBeers = beers.filter(beer =>
+    beer.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Punk API Beers</h1>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search by beer name"
+          value={searchTerm}
+          onChange={event => setSearchTerm(event.target.value)}
+        />
+      </header>
+      <div className="beer-list">
+        {beers && filteredBeers.map(beer => (
+          <div key={beer.id} className="beer-card">
+            <img src={beer.image_url} alt={beer.name} />
+            <h3>{beer.name}</h3>
+            <p>{beer.tagline}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BeerList;
